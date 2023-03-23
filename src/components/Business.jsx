8febfeb7c918be20a717f318de597ed656893bf5
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-03-23 13:53:03 -0700

    Create business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import clsx from 'clsx';
+
+import Button from './Button';
+
+import { features } from '../constants';
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={clsx('flex p-6 rounded-[20px] feature-card', {
+      'mb-6': index !== features.length - 1
+    })}
+  >
+    <div className="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue">
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className="flex md:flex-row flex-col sm:py-16 py-6">
+      <div className="flex-1 flex justify-center items-start flex-col">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          You do the business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
+
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className="flex-1 flex flex-col justify-center items-center md:ml-10 ml-0 md:mt-0 mt-10 relative">
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Business;
